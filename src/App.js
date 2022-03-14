5bfe20db1a4c830cd22aed3da5a3bb45deda62b1
carpemonf <carpemonf@gmail.com>
2022-03-14 18:06:31 +0100

    Adding meta and images for sharing links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@mui/styles";
 import { lightTheme, darkTheme } from './components/theme';
 import { React, useState, useEffect, createContext } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MetaTags from 'react-meta-tags';
 import {
    Navigation,
    Footer,
@@ -38,6 +39,21 @@ function App() {
 
   return (
     <div>
+      <MetaTags>
+        <title>cfd.xyz</title>
+        <meta name="description" content="An open-source web platform for data-driven generation and visualization of CFD data." />
+        <meta property="og:description" content="An open-source web platform for data-driven generation and visualization of CFD data." />
+        <meta property="og:title" content="cfd.xyz" />
+        <meta property="og:image" content="images/card.png" />
+        <meta property="og:url" content="http://www.cfd.xyz" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="cfd.xyz" />
+        <meta property="twitter:url" content="http://www.cfd.xyz" />
+        <meta name="twitter:title" content="cfd.xyz" />
+        <meta name="twitter:description" content="An open-source web platform for data-driven generation and visualization of CFD data." />
+        <meta name="twitter:image" content="images/card.png" />
+      </MetaTags>
       <Router>
         <div className={classes.mainWrapper}>
           <ThemeContext.Provider value={value}>
