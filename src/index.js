90d5fd4e6df8ecab3f5e814cde1bf391ec21bb27
carpemonf <carpemonf@gmail.com>
2022-04-20 12:59:14 +0200

    Changing global font to Varela

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "@fontsource/ibm-plex-sans";
+import "@fontsource/varela";
 
 ReactDOM.render(
   <React.StrictMode>
