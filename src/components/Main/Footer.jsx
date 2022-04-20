e463221263a6086adcd3248f9aed25de279a172e
carpemonf <carpemonf@gmail.com>
2022-04-20 13:17:54 +0200

    Improving style on Footer component

--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -2,7 +2,7 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-import React from "react";
+import React from 'react';
 import { makeStyles } from "@mui/styles";
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
@@ -10,8 +10,13 @@ import { lightTheme, darkTheme } from './../theme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
+const slackInvite = 'zt-15qjacmzo-1woWqeklQ0IeXZb_F6ueaQ'
+const slackLink = 'https://join.slack.com/t/cfd-xyz/shared_invite/' + slackInvite
+const linkedinLink = 'https://www.linkedin.com/sharing/share-offsite/?url=http://www.cfd.xyz'
+const paypalLink = 'https://www.paypal.com/donate/?hosted_button_id=KKB4LH96E59A4'
+
 function Footer() {
-  const themeType = window.localStorage.getItem('theme') || "light"
+  const themeType = window.localStorage.getItem('theme') || 'light'
 
   //const localTheme = window.localStorage.getItem('theme');
   const theme = themeType === 'light' ? lightTheme : darkTheme;
@@ -25,9 +30,9 @@ function Footer() {
       <div>
         <Typography
           style={{
-            fontFamily: 'monospace',
+            fontFamily: 'Varela',
             fontSize: 12,
-            fontWeight: 600
+            fontWeight: 500
           }}
         >
           {'© '}
@@ -36,10 +41,10 @@ function Footer() {
           {'SIMZERO'}
           <Link
             className={classes.link}
-            color="inherit"
-            href="https://www.companyname.com"
-            target="_blank"
-            rel="noreferrer"
+            color='inherit'
+            href='https://www.companyname.com'
+            target='_blank'
+            rel='noreferrer'
           >
           </Link>
         </Typography>
@@ -47,45 +52,142 @@ function Footer() {
     );
   }
 
-  //<div style={{position: "fixed", left: 0, bottom: 0, right: 0}}>
   return (
     <div>
-      <div style={{position: "absolute", left: 0, bottom: 0, right: 0}}>
-        <footer style={{ padding: '0px', bottom: '0', position: 'relative', height: '52px', width: '100%', backgroundColor: mainPrimaryColor, color: mainSecondaryColor }}>
-          <div style={{marginTop: 8, padding: 10, justifyContent: 'center', alignItems: 'center', flexDirection: "column", display: 'inline-block', float: 'left'}}>
+      <div style={{position: 'absolute', left: 0, bottom: 0, right: 0}}>
+        <footer
+          style={{
+            padding: '0px',
+            bottom: '0',
+            position: 'relative',
+            height: '52px',
+            width: '100%',
+            backgroundColor: mainPrimaryColor,
+            color: mainSecondaryColor
+          }}
+        >
+          <div
+            style={{
+              marginTop: 8,
+              padding: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: "column",
+              display: 'inline-block',
+              float: 'left'
+            }}
+          >
             <Copyright />
           </div>
-          <div style={{alignSelf: 'flex-end', position: 'absolute', marginTop: 4, right: 8}}>
-            <div style={{alignSelf: 'flex-end', position: 'absolute', right: 5, padding: 10, margin: '0', width: '15%', display: 'table'}}>
+          <div
+            style={{
+              alignSelf: 'flex-end',
+              position: 'absolute',
+              marginTop: 4,
+              right: 8
+            }}
+          >
+            <div
+              style={{
+                alignSelf: 'flex-end',
+                position: 'absolute',
+                right: 5,
+                padding: 10,
+                margin: '0',
+                width: '15%',
+                display: 'table'
+              }}
+            >
               <div style={{ display: 'table-row'}}>
-                <div style={{width: '600px', display: 'table-cell', paddingLeft: 0}}>
+                <div
+                  style={{
+                    width: '600px',
+                    display: 'table-cell',
+                    paddingLeft: 0
+                  }}
+                >
                   <Typography
-                    variant="body2"
-                    align="center"
+                    variant='body2'
+                    align='center'
                   >
-                    <a className={classes.link} target="_blank" rel="noreferrer" href="https://join.slack.com/t/cfd-xyz/shared_invite/zt-15qjacmzo-1woWqeklQ0IeXZb_F6ueaQ">
-                      <FontAwesomeIcon title="Engage (Slack)" style={{ align: 'center', color:mainPrimaryColor,background: mainSecondaryColor, width: '24px', height: '24px', display: 'inline-block'}} icon={brands('slack')} />
+                    <a
+                      className={classes.link}
+                      target='_blank'
+                      rel='noreferrer'
+                      href={slackLink}
+                    >
+                      <FontAwesomeIcon
+                        title='Engage (Slack)'
+                        style={{ width: '24px', height: '24px'}}
+                        icon={brands('slack')} />
                     </a>
                   </Typography>
                 </div>
-                <div style={{align: 'center', width: '600px', display: 'table-cell', paddingLeft: 10}}>
+                <div
+                  style={{
+                    align: 'center',
+                    width: '600px',
+                    display: 'table-cell',
+                    paddingLeft: 11
+                  }}
+                >
                   <Typography align="center">
-                    <a className={classes.link} target="_blank" rel="noreferrer" href="https://github.com/simzero-oss/cfd-xyz">
-                      <FontAwesomeIcon title="Contribute (GitHub)" style={{ width: '24px', height: '24px'}} icon={brands('github')} />
+                    <a
+                      className={classes.link}
+                      target='_blank'
+                      rel='noreferrer'
+                      href='https://github.com/simzero-oss/cfd-xyz'
+                    >
+                      <FontAwesomeIcon
+                        title='Contribute (GitHub)'
+                        style={{ width: '24px', height: '24px'}}
+                        icon={brands('github')}
+                      />
                     </a>
                   </Typography>
                 </div>
-                <div style={{flexGrow: '1',align: 'center', width: '600px', display: 'table-cell', paddingLeft: 10}}>
-                  <Typography align="center">
-                    <a className={classes.link} target="_blank" rel="noreferrer" href="https://www.linkedin.com/sharing/share-offsite/?url=http://www.cfd.xyz">
-                      <FontAwesomeIcon title="Share (LinkedIn)" style={{width: '24px', height: '24px'}} icon={brands('linkedin')} />
+                <div
+                  style={{
+                    align: 'center',
+                    width: '600px',
+                    display: 'table-cell',
+                    paddingLeft: 11
+                  }}
+                >
+                  <Typography align='center'>
+                    <a
+                      className={classes.link}
+                      target='_blank'
+                      rel='noreferrer'
+                      href={linkedinLink}
+                    >
+                      <FontAwesomeIcon
+                        title='Share (LinkedIn)'
+                        style={{width: '24px', height: '24px'}}
+                        icon={brands('linkedin')}
+                      />
                     </a>
                   </Typography>
                 </div>
-                <div style={{align: 'center', width: '600px', display: 'table-cell', paddingLeft: 10}}>
-                  <Typography align="center">
-                    <a className={classes.link} target="_blank" rel="noreferrer" href="https://www.paypal.com/donate/?hosted_button_id=KKB4LH96E59A4">
-                      <FontAwesomeIcon title="Donate (PayPal)" style={{ width: '24px', height: '24px'}} icon={solid('piggy-bank')} />
+                <div
+                  style={{
+                    align: 'center',
+                    width: '600px',
+                    display: 'table-cell',
+                    paddingLeft: 11
+                  }}
+                >
+                  <Typography align='center'>
+                    <a
+                      className={classes.link}
+                      target='_blank'
+                      rel='noreferrer'
+                      href={paypalLink}>
+                      <FontAwesomeIcon
+                        title="Donate (PayPal)"
+                        style={{ width: '24px', height: '24px'}}
+                        icon={solid('piggy-bank')}
+                      />
                     </a>
                   </Typography>
                 </div>
